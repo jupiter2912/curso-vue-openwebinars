54a7583ab2cfa468c397cf9eccfae35531924d70
Jose <email>
2020-10-19 16:34:54 +0200

    css, postcss (aplicando estilos a los componentes)

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,6 @@
 
 </template>
 
-
-
-
-
 <script>
 /* eslint-disable */
 import CoLogo from '@/components/CoLogo'
@@ -55,6 +51,60 @@ export default {
 }
 </script>
 
+
 <style lang='css'>
+/* css global a la aplicacion */
+	@import 'assets/css/reset';
+	@import 'assets/css/global';
 </style>
+
+/** Podemos crear todos los styles que queramos siempre que identifiquemos el scope */
+/** css de app.vue */
+<style lang="css" scoped>
+	@import './assets/css/colors.css'; /** Importa los colores */
+	@import './assets/css/mixins.css';
+
+/** post css */
+/** creamos una clase codit */
+	.codit {
+		@apply --flex-col; /** aplicas el mixing flex-col */
+
+		align-items: center;
+		justify-content: center;
+
+		width: 100vw;
+		height: 100vh;
+
+		padding: 1px;
+		
+		word-break: break-word;
+	}
+	
+
+		  .container {
+			@apply --flex-row;
+			background-color: var(--color-lighter-grey);
+			width: 100%;
+			height: 100%;
+			max-width: 80vh;
+			min-width: 400px;
+			max-height: 90vh;
+			min-height: 300px;
+			}
+			 .menu {
+			@apply --flex-col;
+			max-width: 15rem;
+			background-color: var(--color-blue);
+			}
+			 .content {
+			@apply --flex-col;
+			flex: 1;
+			}
+			 .content__header {
+			padding: 1rem;
+			text-align: center;
+			background-color: var(--color-blue);
+		}
+	
+		</style>
 
--- src/components/CoLogo.vue
+++ src/components/CoLogo.vue
@@ -15,6 +15,21 @@
 	}
   }
 </script>
+/** Con scoped conseguimos que el css se encapsule */
+ <style lang='css' scoped> 
 
-<style lang='css'>
+	@import '../assets/css/colors.css';
+
+	.logo {
+		margin: 0;
+
+		color: var(--color-lighter-grey);
+
+		line-height: 2rem;
+		font-size: 2rem;
+		font-weight: 800;
+		letter-spacing: -2px;
+
+		text-shadow: 0 0 20px color(var(--color-blue));
+	}
 </style>
