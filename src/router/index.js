ef5a50a6420f9df4ff197d091c736b937ff72f3a
Jose <email>
2020-10-20 12:54:42 +0200

    instanciamiento de router

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+// definicion de las rutas
+/* eslint-disable */
+import Vue from 'vue'
+
+import Router from 'vue-router'
+
+Vue.use(Router) // el plugin de router se inyecta en Vue. Cada componente tendra acceso al router
+
+export default new Router({
+	mode: 'history',
+	routes: { 
+			//Definiremos las rutas
+	}
+})
